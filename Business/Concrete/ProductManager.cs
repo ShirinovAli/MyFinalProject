b0ef561034f44da45dfa9d04498d2219e2bf2983
ali-shirinov@hotmail.com <ali-shirinov@hotmail.com>
2021-03-12 19:00:19 +0400

    12.03.2021 - second

--- Business/Concrete/ProductManager.cs
+++ Business/Concrete/ProductManager.cs
@@ -4,6 +4,7 @@ using Business.Constants;
 using Business.ValidationRules.FluentValidation;
 using Core.Aspects.Autofac.Validation;
 using Core.CrossCuttingConcerns.Validation;
+using Core.Utilities.Business;
 using Core.Utilities.Results;
 using DataAccess.Abstract;
 using Entities.Concrete;
@@ -19,24 +20,30 @@ namespace Business.Concrete
     public class ProductManager : IProductService
     {
         IProductDal _productDal;
-        public ProductManager(IProductDal productDal)
+        ICategoryService _categoryService;
+
+        public ProductManager(IProductDal productDal, ICategoryService categoryService)
         {
             _productDal = productDal;
+            _categoryService = categoryService;
+
         }
 
         [ValidationAspect(typeof(ProductValidator))]
         public IResult Add(Product product)
         {
-            //business codes
-            if (CheckIfProductCountOfCategoryCorrect(product.CategoryId).Success)
+            IResult result = BusinessRules.Run(CheckIfProductCountOfCategoryCorrect(product.CategoryId), 
+                                               CheckIfProductNameExists(product.ProductName), 
+                                               CheckIfCategoryLimitExceded());
+
+            if (result != null)
             {
-                if (CheckIfProductNameExists(product.ProductName).Success)
-                {
-                    _productDal.Add(product);
-                    return new SuccessResult(Messages.ProductAdded);
-                }
+                return result;
             }
-            return new ErrorResult();
+
+            _productDal.Add(product);
+
+            return new SuccessResult(Messages.ProductAdded);
         }
 
         public IDataResult<List<Product>> GetAll()
@@ -99,6 +106,16 @@ namespace Business.Concrete
             if (result)
             {
                 return new ErrorResult(Messages.ProductNameAlreadyExists);
+            }
+            return new SuccessResult();
+        }
+
+        private IResult CheckIfCategoryLimitExceded()
+        {
+            var result = _categoryService.GetAll();
+            if (result.Data.Count > 15)
+            {
+                return new ErrorResult(Messages.CategoryLimitExceded);
             }
             return new SuccessResult();
         }
--- Business/Constants/Messages.cs
+++ Business/Constants/Messages.cs
@@ -12,6 +12,7 @@ namespace Business.Constants
         public static string MaintenanceTime = "Sistem texniki qulluqda";
         public static string ProductListed = "Mehsullar listelendi";
         public static string ProductCountOfCategoryError = "Bir kategoriyada max 10 mehsul ola biler";
-        internal static string ProductNameAlreadyExists = "Bu adda mehsul artiq movcuddur";
+        public static string ProductNameAlreadyExists = "Bu adda mehsul artiq movcuddur";
+        public static string CategoryLimitExceded = "Kategoriya limiti kecdiyi ucun yeni mehsul elave edilmedi";
     }
 }
