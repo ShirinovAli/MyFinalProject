5279a29845ee8d6b2318552004946f7c32253fd7
ali-shirinov@hotmail.com <ali-shirinov@hotmail.com>
2021-03-13 02:46:49 +0400

    13.03.21 commit 1

--- Business/Concrete/ProductManager.cs
+++ Business/Concrete/ProductManager.cs
@@ -1,4 +1,5 @@
 ﻿using Business.Abstract;
+using Business.BusinessAspects.Autofac;
 using Business.CCS;
 using Business.Constants;
 using Business.ValidationRules.FluentValidation;
@@ -29,11 +30,12 @@ namespace Business.Concrete
 
         }
 
+        [SecuredOperation("product.add,admin")]
         [ValidationAspect(typeof(ProductValidator))]
         public IResult Add(Product product)
         {
-            IResult result = BusinessRules.Run(CheckIfProductCountOfCategoryCorrect(product.CategoryId), 
-                                               CheckIfProductNameExists(product.ProductName), 
+            IResult result = BusinessRules.Run(CheckIfProductCountOfCategoryCorrect(product.CategoryId),
+                                               CheckIfProductNameExists(product.ProductName),
                                                CheckIfCategoryLimitExceded());
 
             if (result != null)
--- Business/Constants/Messages.cs
+++ Business/Constants/Messages.cs
@@ -1,6 +1,8 @@
-﻿using Entities.Concrete;
+﻿using Core.Entities.Concrete;
+using Entities.Concrete;
 using System;
 using System.Collections.Generic;
+using System.Runtime.Serialization;
 using System.Text;
 
 namespace Business.Constants
@@ -14,5 +16,12 @@ namespace Business.Constants
         public static string ProductCountOfCategoryError = "Bir kategoriyada max 10 mehsul ola biler";
         public static string ProductNameAlreadyExists = "Bu adda mehsul artiq movcuddur";
         public static string CategoryLimitExceded = "Kategoriya limiti kecdiyi ucun yeni mehsul elave edilmedi";
+        public static string AuthorizationDenied = "Icazeniz yoxdur";
+        public static string UserRegistered = "Ugurla qeydiyyatdan kecdiniz";
+        public static string UserNotFound = "Istifadeci tapilmadi";
+        public static string PasswordError = "Sifre yanlisdir";
+        public static string SuccessfulLogin = "Ugurlu daxil olma";
+        public static string UserAlreadyExists = "Bele bir istifadeci artiq movcuddur";
+        public static string AccessTokenCreated = "Access token ugurla yaradildi";
     }
 }
